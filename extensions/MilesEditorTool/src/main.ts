4e24fe90341d3ed28887c3a2bdf0f65524b358c7
miles <813601376@qq.com>
2023-04-25 15:01:18 +0800

    update

---
+++ extensions/MilesEditorTool/src/main.ts
@@ -0,0 +1,23 @@
+
+/**
+ * @en Registration method for the main process of Extension
+ * @zh 为扩展的主进程的注册方法
+ */
+export const methods: { [key: string]: (...any: any) => any } = {
+    log(){
+        Editor.Dialog.info("hahaha");
+        console.log("hahaha");
+    }
+};
+
+/**
+ * @en Hooks triggered after extension loading is complete
+ * @zh 扩展加载完成后触发的钩子
+ */
+export function load() { }
+
+/**
+ * @en Hooks triggered after extension uninstallation is complete
+ * @zh 扩展卸载完成后触发的钩子
+ */
+export function unload() { }
