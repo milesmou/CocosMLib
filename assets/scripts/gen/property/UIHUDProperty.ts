d5aa5302588cc8da3fd7d06d2144560319ab5d05
Miles <miles.emmm@gmail.com>
2023-12-01 18:38:49 +0800

    update

--- assets/scripts/gen/property/UIHUDProperty.ts
+++ assets/scripts/gen/property/UIHUDProperty.ts
@@ -2,5 +2,4 @@ import * as cc from "cc";
 import { PropertyBase } from "../../../mlib/module/ui/property/PropertyBase";
 
 export class UIHUDProperty extends PropertyBase {
-    public get node222Tf() { return this.getComp("$Node222", cc.UITransform); }
 }
