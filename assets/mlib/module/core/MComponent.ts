033a75c63713c2102a9c36ff1f94d8b2dc5373f0
Miles <miles.emmm@gmail.com>
2024-01-29 20:14:02 +0800

    update

---
+++ assets/mlib/module/core/MComponent.ts
@@ -0,0 +1,11 @@
+import { _decorator, Component } from "cc";
+
+const { ccclass, property } = _decorator;
+
+@ccclass
+export class MComponent extends Component {
+
+    protected __preload(): void {
+        
+    }
+}
---
+++ assets/mlib/module/core/MScene.ts
@@ -0,0 +1,8 @@
+import { _decorator, Component } from "cc";
+
+const { ccclass, property } = _decorator;
+
+@ccclass
+export class MScene extends Component {
+    
+}
