8ae3a962c423038e42a06a6a631229ef8b6728a1
Miles <miles.emmm@gmail.com>
2024-01-30 14:57:30 +0800

    Update TimerComponent.ts

--- assets/mlib/module/core/TimerComponent.ts
+++ assets/mlib/module/core/TimerComponent.ts
@@ -1,11 +1,13 @@
-import { _decorator, Animation, Component, Scheduler, sp } from "cc";
+import { _decorator, Animation, AnimationManager, AnimationState, Component, director, Scheduler, sp } from "cc";
 
 const { ccclass, property } = _decorator;
 
 @ccclass
 export class TimerComponent extends Component {
+    private _timeScale: number = 1;
     private _scheduler: Scheduler;
     private _pause: boolean = false;
+    private _animationManager: AnimationManager;
     private _animations: Set<Animation> = new Set();
     private _skeletons: Set<sp.Skeleton> = new Set();
     private _updateName = "update";
@@ -13,15 +15,16 @@ export class TimerComponent extends Component {
 
     protected onLoad(): void {
         this._scheduler = new Scheduler();
+        this._animationManager = new AnimationManager();
     }
 
     public setTimeScale(timeScale: number) {
+        this._timeScale = timeScale;
         if (this._scheduler) this._scheduler.setTimeScale(timeScale);
     }
 
     public getTimeScale() {
-        if (this._scheduler) return this._scheduler.getTimeScale();
-        return 0;
+        return this._timeScale;
     }
 
     public pause() {
@@ -38,22 +41,52 @@ export class TimerComponent extends Component {
         if (obj instanceof Animation) {
             if (this._animations.has(obj)) return;
             this._animations.add(obj);
+            this.swapAnimationManager(obj, true);
         } else if (obj instanceof sp.Skeleton) {
             if (this._skeletons.has(obj)) return;
             this._skeletons.add(obj)
+            this.swapUpdateFunction(obj);
         }
-        this.swapUpdateFunction(obj);
     }
 
     /** 移除对象 改为由引擎管理播放速度 */
     public delete(obj: Animation | sp.Skeleton) {
-        let result = false;
         if (obj instanceof Animation) {
-            result = this._animations.delete(obj);
+            if (this._animations.delete(obj)) {
+                this.swapAnimationManager(obj, false)
+            }
         } else if (obj instanceof sp.Skeleton) {
-            result = this._skeletons.delete(obj);
+            if (this._skeletons.delete(obj)) {
+                this.swapUpdateFunction(obj);
+            }
         }
-        if (result) this.swapUpdateFunction(obj);
+    }
+
+    private getAnimationStates(anim: Animation) {
+        let states: AnimationState[] = [];
+        for (const clip of anim.clips) {
+            let state = anim.getState(clip.name);
+            if (state) states.push(state);
+        }
+        if (anim.defaultClip) {
+            let state = anim.getState(anim.defaultClip.name);
+            if (!states.includes(state)) states.push(state);
+        }
+        return states;
+    }
+
+    private swapAnimationManager(anim: Animation, isAdd: boolean) {
+        let sysAnimationManager = director.getSystem(AnimationManager.ID) as AnimationManager;
+        let states = this.getAnimationStates(anim);
+        for (const state of states) {
+            if (isAdd) {
+                sysAnimationManager.removeAnimation(state);
+                this._animationManager.addAnimation(state);
+            } else {
+                sysAnimationManager.addAnimation(state);
+                this._animationManager.removeAnimation(state);
+            }
+        }
     }
 
     private swapUpdateFunction(obj: Component) {
@@ -83,21 +116,12 @@ export class TimerComponent extends Component {
         if (this._pause) return;
         if (this._scheduler) {
             this._scheduler.update(dt);
-            this._animations.forEach(v => {
-                if (v?.isValid) {
-                    let func: Function = v[this._manualUpdateName];
-                    if (typeof func === "function") {
-                        func.call(v, dt * this._scheduler.getTimeScale());
-                    }
-                } else {
-                    this._animations.delete(v);
-                }
-            });
+            this._animationManager.update(dt * this._timeScale);
             this._skeletons.forEach(v => {
                 if (v?.isValid) {
                     let func: Function = v[this._manualUpdateName];
                     if (typeof func === "function") {
-                        func.call(v, dt * this._scheduler.getTimeScale());
+                        func.call(v, dt * this._timeScale);
                     }
                 } else {
                     this._skeletons.delete(v);
