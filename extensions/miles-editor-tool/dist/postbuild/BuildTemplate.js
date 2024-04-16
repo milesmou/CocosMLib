eca68245ad052f2244639f7f200089963675750b
Miles <miles.emmm@gmail.com>
2024-04-16 15:02:09 +0800

    update

--- extensions/miles-editor-tool/dist/postbuild/BuildTemplate.js
+++ extensions/miles-editor-tool/dist/postbuild/BuildTemplate.js
@@ -22,17 +22,26 @@ class BuildTemplate {
         let templatePath = Utils_1.Utils.ProjectPath + "/" + Constant_1.Constant.BuildTemplateDirName + "/" + options.platform;
         fs_extra_1.default.ensureDirSync(templatePath);
         //拷贝模板目录资源
-        let insertPrefix = "insert_"; //以这个前缀开头的文件 会将构建模版中的内容插入到构建后的文件开头
+        let insertPrefix = "insert_"; //以这个前缀开头的文件 会将构建模版中的内容插入到构建后的文件指定行
         let buildPath = Utils_1.Utils.toUniSeparator(result.dest);
         let files = Utils_1.Utils.getAllFiles(templatePath);
         let buildDest = this.resolveBuildDest(buildPath, options.platform);
         for (const file of files) {
             let f = file.replace(templatePath, "").replace("/", "");
-            if (f.startsWith("insert_")) {
-                f = f.replace("insert_", "");
+            if (f.startsWith(insertPrefix)) {
+                f = f.replace(insertPrefix, "");
+                let lineNum = 0;
+                if (f.includes("_")) {
+                    let str = f.substring(0, f.indexOf("_"));
+                    let num = parseInt(str);
+                    if (!isNaN(num)) {
+                        lineNum = num;
+                        f = f.replace(str + "_", "");
+                    }
+                }
                 let newFile = buildDest + "/" + f;
                 LogToFile_1.LogToFile.log("insert code ", f);
-                this.insertCode(file, newFile);
+                this.insertCode(file, newFile, lineNum);
             }
             else {
                 let newFile = buildDest + "/" + f;
@@ -43,10 +52,19 @@ class BuildTemplate {
         }
     }
     /** 如果构建模板中有特殊脚本 插入内容到构建出的文件内容开头 */
-    static insertCode(src, dest) {
+    static insertCode(src, dest, lineNum) {
         let code = fs_extra_1.default.readFileSync(src, { encoding: "utf8" });
+        let codeArr = code.split("\r\n");
+        if (codeArr.length < 2) {
+            codeArr = code.split("\n");
+        }
         let destContent = fs_extra_1.default.readFileSync(dest, { encoding: "utf8" });
-        fs_extra_1.default.writeFileSync(dest, code + "\n" + destContent);
+        let destArr = destContent.split("\r\n");
+        if (destArr.length < 2) {
+            destArr = destContent.split("\n");
+        }
+        destArr.splice(lineNum, 0, ...codeArr);
+        fs_extra_1.default.writeFileSync(dest, destArr.join("\n"));
     }
     static resolveBuildDest(buildDest, platform) {
         // if (platform == "android") {
--- extensions/miles-editor-tool/src/postbuild/BuildTemplate.ts
+++ extensions/miles-editor-tool/src/postbuild/BuildTemplate.ts
@@ -18,17 +18,26 @@ export class BuildTemplate {
         let templatePath = Utils.ProjectPath + "/" + Constant.BuildTemplateDirName + "/" + options.platform;
         fs.ensureDirSync(templatePath);
         //拷贝模板目录资源
-        let insertPrefix = "insert_";//以这个前缀开头的文件 会将构建模版中的内容插入到构建后的文件开头
+        let insertPrefix = "insert_";//以这个前缀开头的文件 会将构建模版中的内容插入到构建后的文件指定行
         let buildPath = Utils.toUniSeparator(result.dest);
         let files = Utils.getAllFiles(templatePath);
         let buildDest = this.resolveBuildDest(buildPath, options.platform);
         for (const file of files) {
             let f = file.replace(templatePath, "").replace("/", "");
-            if (f.startsWith("insert_")) {
-                f = f.replace("insert_", "");
+            if (f.startsWith(insertPrefix)) {
+                f = f.replace(insertPrefix, "");
+                let lineNum = 0;
+                if (f.includes("_")) {
+                    let str = f.substring(0, f.indexOf("_"));
+                    let num = parseInt(str);
+                    if (!isNaN(num)) {
+                        lineNum = num;
+                        f = f.replace(str + "_", "");
+                    }
+                }
                 let newFile = buildDest + "/" + f;
                 LogToFile.log("insert code ", f);
-                this.insertCode(file, newFile);
+                this.insertCode(file, newFile, lineNum);
             } else {
                 let newFile = buildDest + "/" + f;
                 LogToFile.log("copy file", f);
@@ -39,10 +48,20 @@ export class BuildTemplate {
     }
 
     /** 如果构建模板中有特殊脚本 插入内容到构建出的文件内容开头 */
-    private static insertCode(src: string, dest: string) {
+    private static insertCode(src: string, dest: string, lineNum: number) {
         let code = fs.readFileSync(src, { encoding: "utf8" });
+        let codeArr = code.split("\r\n");
+        if (codeArr.length < 2) {
+            codeArr = code.split("\n");
+        }
         let destContent = fs.readFileSync(dest, { encoding: "utf8" });
-        fs.writeFileSync(dest, code + "\n" + destContent);
+        let destArr = destContent.split("\r\n");
+        if (destArr.length < 2) {
+            destArr = destContent.split("\n");
+        }
+        destArr.splice(lineNum, 0, ...codeArr);
+
+        fs.writeFileSync(dest, destArr.join("\n"));
     }
 
 
