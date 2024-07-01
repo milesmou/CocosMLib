eab334de42abe70a59371e157bf3a594a291e37a
Amun <amunemmm@gmail.com>
2024-07-01 15:10:05 +0800

    Update ObjectPoolArgs.ts

--- assets/mlib/module/pool/ObjectPoolArgs.ts
+++ assets/mlib/module/pool/ObjectPoolArgs.ts
@@ -1,10 +1,10 @@
 /** 对象池创建参数 */
-export class ObjectPoolArgs<T extends object = object> {
+export interface ObjectPoolArgs<T extends object = object> {
 
     /** 初始化时创建对象数量 */
     defaultCreateNum: number;
 
-    /** 对象迟数量不足时创建新对象 */
+    /** 对象池数量不足时创建新对象 */
     newObject: () => T;
 
     /** 获取对象时的处理 */
