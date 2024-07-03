717ad893dd9f44d83f9797d0cbf35066d12faf81
Amun <amunemmm@gmail.com>
2024-07-03 16:24:30 +0800

    Update Decorator.ts

--- assets/mlib/module/core/Decorator.ts
+++ assets/mlib/module/core/Decorator.ts
@@ -4,7 +4,7 @@ import { EDITOR } from "cc/env";
 
 /** 将组件添加到常驻节点上 */
 export function persistNode<T extends Component>(target: { Inst: T, new(): T }) {
-    director.once(Director.EVENT_AFTER_SCENE_LAUNCH, () => {
+    director.on(Director.EVENT_AFTER_SCENE_LAUNCH, () => {
         if (EDITOR) return;
         let nodeName = `[${js.getClassName(target)}]`;
         let scene = director.getScene();
