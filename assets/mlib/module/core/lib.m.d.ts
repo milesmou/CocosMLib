16ec2574fef932c6cd85af2de0c72680b9c5dbeb
Amun <amunemmm@gmail.com>
2024-07-11 14:54:59 +0800

    Update lib.m.d.ts

--- assets/mlib/module/core/lib.m.d.ts
+++ assets/mlib/module/core/lib.m.d.ts
@@ -13,22 +13,29 @@ declare module "cc" {
 
         /** 确保组件存在 不存在则添加 */
         ensureComponent<T extends Component>(classConstructor: new (...args: any[]) => T): T;
+
     }
 
     interface Node {
+
         /** 确保组件存在 不存在则添加 */
         ensureComponent<T extends Component>(classConstructor: new (...args: any[]) => T): T;
+
         /** 
          * 节点尺寸匹配父节点大小(通过widget组件来完成)
          * @param immediately 是否立即生效，只有当你需要在当前帧结束前生效才传true，默认为false
          */
         matchParent(immediately?: boolean): void;
+
         /** 获取节点在场景树的路径 */
         getPath(): void;
+
         /** 根据zIndex的值更新子节点的SiblingIndex */
         regularSiblingIndex(): void;
+
         /** 模拟2.x中zIndex,刷新层级需要调用父节点的regularSiblingIndex方法 */
         zIndex: number;
+
         /** 在子节点zIndex值改变时修改父节点此属性为true，表示需要更新子节点的SiblingIndex */
         childrenSiblingIndexDirty: boolean;
     }
@@ -38,6 +45,7 @@ declare global {
 
     /** 应用程序管理单例 */
     const app: App;
+    
     /** 日志打印类 */
     const logger: typeof MLogger;
 
@@ -56,22 +64,27 @@ declare global {
         * 第一个元素
         */
         get first(): T | undefined;
+        
         /** 
          * 最后一个元素
          */
         get last(): T | undefined;
+        
         /**
          * 从数组中删除一个元素
          */
         delete<T>(item: T): boolean;
+        
         /**
          * 从数组中删除一个元素
          */
         delete<T>(predicate: (value: T, index: number, obj: T[]) => boolean): boolean;
+        
         /**
         * 从数组中随机返回一个值，并将它从数组中移除
         */
         random(): T | undefined;
+        
         /**
          * 数组随机打乱
          */
@@ -83,6 +96,7 @@ declare global {
          * 首字母大写
          */
         upperFirst(): string;
+        
         /**
          * 首字母小写 
          */
