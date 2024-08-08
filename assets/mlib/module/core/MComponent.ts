a5117d25490ab2cb0ff58292b6c2e3a9d3b1b782
Amun <amunemmm@gmail.com>
2024-08-08 09:54:36 +0800

    Update MComponent.ts

--- assets/mlib/module/core/MComponent.ts
+++ assets/mlib/module/core/MComponent.ts
@@ -1,10 +1,10 @@
-import { _decorator, Component, js } from "cc";
+import { _decorator, Component } from "cc";
 import { AssetComponent } from "../asset/AssetComponent";
 import { AudioComponent } from "../audio/AudioComponent";
 import { TimerComponent } from "../timer/TimerComponent";
 import { ReferenceCollector } from "./ReferenceCollector";
 
-const { ccclass, property, requireComponent } = _decorator;
+const { ccclass } = _decorator;
 
 @ccclass
 export class MComponent extends Component {
@@ -36,23 +36,24 @@ export class MComponent extends Component {
         }
     }
 
-    /** 调用任意父节点componentName组件的methodName方法 */
-    public sendMessageUpward(componentName: string, methodName: string, ...args: any[]) {
-        let compConstructor: any = js.getClassByName(componentName);
-        let comp = this.getComponentInParent(compConstructor, false);
-        if (comp) {
-            let method: Function = comp[methodName];
-            if (method && typeof method === "function") {
-                method.apply(comp, args);
-            } else {
-                logger.error(`父节点上组件未找到指定方法 ${methodName} NodeName=${comp.node.name}`);
+    /** 调用自身以及父节点上所有MComponent中的methodName方法 */
+    public sendMessageUpwards(methodName: string, ...args: any[]) {
+        let send = (comps: MComponent[]) => {
+            for (const comp of comps) {
+                let method: Function = comp[methodName];
+                if (method && typeof method === "function") {
+                    method.apply(comp, args);
+                }
             }
-        } else {
-            logger.error(`父节点上未找到指定组件 ${componentName}`);
+        }
+        let n = this.node;
+        while (n) {
+            send(n.getComponents(MComponent));
+            n = n.parent;
         }
     }
 
-    /** 广播消息 调用自身以及子节点上所有MComponent中的methodName方法 */
+    /** 调用自身以及子节点上所有MComponent中的methodName方法 */
     public broadcastMessage(methodName: string, ...args: any[]) {
         let comps = this.getComponents(MComponent);
         comps = comps.concat(this.getComponentsInChildren(MComponent));
