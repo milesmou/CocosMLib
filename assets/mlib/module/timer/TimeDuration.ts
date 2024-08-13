09e8edde972295652304d730788344ddcaaf7244
Amun <amunemmm@gmail.com>
2024-08-13 11:52:20 +0800

    Update TimeDuration.ts

--- assets/mlib/module/timer/TimeDuration.ts
+++ assets/mlib/module/timer/TimeDuration.ts
@@ -1,26 +1,23 @@
 /** 时长统计工具类 */
 export class TimeDuration {
 
-    private static inst: TimeDuration;
-    public static get Inst() { return this.inst || (this.inst = new TimeDuration()) }
-
-    private _startTimeMS: Map<string, number> = new Map();
+    private static startTimeMS: Map<string, number> = new Map();
 
     /** 给予一个Key，并开始计时 */
-    public time(key: string) {
-        if (this._startTimeMS.has(key)) {
+    public static time(key: string) {
+        if (this.startTimeMS.has(key)) {
             console.warn("Key已存在", key);
         }
-        this._startTimeMS.set(key, Date.now());
+        this.startTimeMS.set(key, Date.now());
     }
 
     /** 结束指定Key的计时，并返回时长 */
-    public timeEnd(key: string) {
-        if (!this._startTimeMS.has(key)) {
+    public static timeEnd(key: string) {
+        if (!this.startTimeMS.has(key)) {
             console.error("Key不存在", key);
             return 0;
         }
-        let start = this._startTimeMS.get(key);
+        let start = this.startTimeMS.get(key);
         return Date.now() - start;
     }
 }
