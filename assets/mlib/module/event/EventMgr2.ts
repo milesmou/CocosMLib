44977d8810a63cbb2bc695c953b9d380d3a90554
MILES <miles.emmm@gmail.com>
2024-09-04 19:28:45 +0800

    Update EventMgr2.ts

--- assets/mlib/module/event/EventMgr2.ts
+++ assets/mlib/module/event/EventMgr2.ts
@@ -1,44 +1,87 @@
 import { MEvent } from "./MEvent";
 
-export interface EventMap {
-    [eventName: string]: (...args: any) => void
-}
+/** 全局事件管理工具 */
+export class EventMgr2 {
+    private static eventMap: Map<Function, MEvent> = new Map();
+    private static strEventMap: Map<string, MEvent> = new Map();
 
-export class EventMgr2<Events extends EventMap> {
-
-    private _events: Map<string, MEvent> = new Map();
-
-    public on<K extends keyof Events, C extends (...args: Parameters<Events[K]>) => void>(name: K, callback: C, thisObj?: object) {
-        let event = this._events.get(name as string);
-        if (!event) {
-            event = new MEvent();
-            this._events.set(name as string, event);
-        }
-        event.addListener(callback as any, thisObj, false);
+    /** 清理所有事件 */
+    public static clear() {
+        EventMgr2.eventMap.clear();
+        EventMgr2.strEventMap.clear();
     }
 
-    public once<K extends keyof Events, C extends (...args: Parameters<Events[K]>) => void>(name: K, callback: C, thisObj?: object) {
-        let event = this._events.get(name as string);
-        if (!event) {
-            event = new MEvent();
-            this._events.set(name as string, event);
+    /** 为事件注册一个回调,重复注册只保留第一次的事件 */
+    public static on<T extends (...args: any[]) => void, C extends (...args: Parameters<T>) => void>(event: T, callback: C, thisObj?: object): void;
+    public static on(name: string, callback: (...args: any[]) => void, thisObj?: object): void;
+    public static on(arg1: string | Function, callback: (...args: any[]) => void, thisObj?: object): void {
+        let event: MEvent;
+        if (typeof arg1 === "string") {
+            event = this.strEventMap.get(arg1);
+            if (!event) {
+                event = new MEvent();
+                this.strEventMap.set(arg1, event);
+            }
+        } else {
+            event = this.eventMap.get(arg1);
+            if (!event) {
+                event = new MEvent();
+                this.eventMap.set(arg1, event);
+            }
         }
-        event.addListener(callback as any, thisObj, true);
+        event.addListener(callback, thisObj, false);
     }
 
-    public off<K extends keyof Events, C extends (...args: Parameters<Events[K]>) => void>(name: K, callback?: C, thisObj?: object) {
-        let event = this._events.get(name as string);
+    /** 为事件注册一个回调,回调仅会触发一次,重复注册只保留第一次的事件 */
+    public static once<T extends (...args: any[]) => void, C extends (...args: Parameters<T>) => void>(event: T, callback: C, thisObj?: object): void;
+    public static once(name: string, callback: (...args: any[]) => void, thisObj?: object): void;
+    public static once(arg1: string | Function, callback: (...args: any[]) => void, thisObj?: object): void {
+        let event: MEvent;
+        if (typeof arg1 === "string") {
+            event = this.strEventMap.get(arg1);
+            if (!event) {
+                event = new MEvent();
+                this.strEventMap.set(arg1, event);
+            }
+        } else {
+            event = this.eventMap.get(arg1);
+            if (!event) {
+                event = new MEvent();
+                this.eventMap.set(arg1, event);
+            }
+        }
+        event.addListener(callback, thisObj, true);
+    }
+
+    /** 取消事件的某个回调，callback不传值时取消事件所有回调*/
+    public static off<T extends (...args: any[]) => void, C extends (...args: Parameters<T>) => void>(event: T, callback?: C, thisObj?: object): void;
+    public static off(name: string, callback?: (...args: any[]) => void, thisObj?: object): void;
+    public static off(arg1: string | Function, callback?: (...args: any[]) => void, thisObj?: object): void {
+        let event: MEvent;
+        if (typeof arg1 === "string") {
+            event = this.strEventMap.get(arg1);
+        } else {
+            event = this.eventMap.get(arg1);
+        }
         if (event) {
             if (callback) {
-                event.removeListener(callback as any, thisObj);
+                event.removeListener(callback, thisObj);
             } else {
                 event.removeAllListeners();
             }
         }
     }
 
-    public emit<K extends keyof Events, D extends Parameters<Events[K]>>(name: K, ...args: D) {
-        let event = this._events.get(name as any);
+    /** 触发事件，参数个数不固定 */
+    public static emit<T extends (...args: any[]) => void, P extends Parameters<T>>(event: T, ...args: P): void;
+    public static emit(name: string, ...args: any[]): void;
+    public static emit(arg1: string | Function, ...args: any[]): void {
+        let event: MEvent;
+        if (typeof arg1 === "string") {
+            event = this.strEventMap.get(arg1);
+        } else {
+            event = this.eventMap.get(arg1);
+        }
         if (event) {
             event.dispatch(...args);
         }
